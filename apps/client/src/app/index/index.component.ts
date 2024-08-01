e30f692da7d9ab552bca1fd6197d3e391fc211b7
TheKenkei <nowikow.witaliy@gmail.com>
2024-08-01 21:08:14 +0300

    feat: add new

---
+++ apps/client/src/app/index/index.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {CommonModule, NgFor} from '@angular/common';
+
+@Component({
+    selector: 'app-index',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './index.component.html',
+    styleUrl: './index.component.css',
+})
+export class IndexComponent {}
